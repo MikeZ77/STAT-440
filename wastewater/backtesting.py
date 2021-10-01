35d0b6032adbdb50e64e255c1f29c1316a4c2030
mzaghi <mzaghi@sfu.ca>
2021-09-30 20:06:18 -0700

    Redifining how models run and adding more methods. Bug with predictions >1 remains.

--- wastewater/backtesting.py
+++ wastewater/backtesting.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+import os
 from datetime import datetime, timedelta
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import KFold
-from sklearn.impute import SimpleImputer
 
 class Backtest:
   """
@@ -13,11 +14,18 @@ class Backtest:
     self.clean_data()
     self.predictions_info = pd.read_csv('data/predictions.csv')['Date:Delay'].to_list()
     self.predictions = pd.read_csv('data/predictions.csv')
+    self.predictions['Actual'] = 0 #For calculating RMSPE
 
-  def __call__(self, model):
+  def __call__(self, output_predication_csv = False, **kwargs):
     """
     Backtests model for every prediction.
+    No forward contamination
+    Method: 
+    1. Shift the target 'New cases' up T - D days
+    2. Get the T - D day and create X_prime
+    3. From the original data test against T
     """
+    model = kwargs['model']
     for items in self.predictions_info:
       items = items.split(':')
       T = items[0]
@@ -29,47 +37,94 @@ class Backtest:
       X_prime = self.data.copy() 
       X_prime['New cases'] = X_prime['New cases'].shift(-D)
 
-
+      # idx is the index of the last row of X'
       idx = X_prime.index[X_prime['Date'] == T_prime].tolist()[0]
       X_prime = X_prime[0:idx+1]
 
-      X = X_prime.loc[:, 'Cumulative cases':'Active cases']
-      Y = X_prime['New cases']
+      # Convert to numpy arrays
+      X_train = X_prime.loc[:, 'Cumulative cases':'Active cases'].to_numpy()
+      X_test = self.data.loc[idx+1, 'Cumulative cases':'Active cases'].to_numpy().reshape(1, -1) # Fix this (idx)
+      Y_train = X_prime['New cases'].to_numpy()
+      Y_test = self.data.loc[idx+1, 'New cases'] # Fix this (idx)
 
-      self.evaluate_model(X, Y, model)
+      Y_hat = None
+      if model == 'regression':
+        Y_hat = self.run_regression(X_train, Y_train, X_test)
+      else:
+        print('Please pass a model to train and evaluate.')
+      
+      self.add_prediction(Y_hat, Y_test, T, D)
+      
 
-      break
+    RMSPE = self.RMSPE()
+    if output_predication_csv: self.output_csv(model)
+    return RMSPE
 
-  def evaluate_model(self, X, Y, model):
+  def run_regression(self, X_train, Y_train, X_test) -> float:
     """
-    Trains and tests model using k-fold cross validation.
+    Returns a prediction using standard linear regression
     """
-    kf = KFold(n_splits=4)
+    model = LinearRegression()
+    model.fit(X_train, Y_train)
+    Y_hat = model.predict(X_test)[0]
+    return Y_hat
+
+  def evaluate_model(self, X, num = 4) -> list:
+    """
+    Helper function. Returns a list of tuples containg the data frame X split num ways
+    Output: [(X_train, X_test) ...]
+    """
+    output = list()
+    kf = KFold(n_splits = num)
     kf.get_n_splits(X)
     for train_index, test_index in kf.split(X):
       X_train = X.iloc[train_index]
-      Y_train = Y.iloc[train_index]
       X_test = X.iloc[test_index]
-      Y_test = Y.iloc[test_index].to_numpy()
+      output.append((X_train, X_test))
+    return output
 
-      model.fit(X_train, Y_train)
-      Y_hat = model.predict(X_test)
-
-      # RMSE
-      # print(np.sqrt(np.mean((Y_hat-Y_test)**2))) 
-
-
+  def RMSPE(self) -> float:
+    """
+    Root Mean Squared Predication Error
+    """
+    # Count => Y_hat, Actual => Y
+    return np.sqrt(np.mean((self.predictions['Count'] - self.predictions['Actual'])**2))
+    
   def clean_data(self):
     """
-    Missing data on the first 17 days are zero'd.
-    Fill forward is performed on the remaining missing days (weekends)
+    Cleans the given data set.
+    Methodology:
+    1. Missing data on the first 17 days are zero'd.
+    2. Forward fill is performed on the remaining missing days (weekends)
     """
     self.data[0:18] = self.data[0:18].fillna(value=0)
     self.data = self.data.fillna(method='ffill')
 
+  def add_prediction(self, Y_hat, Y_test, T, D):
+    """
+    Adds the predication of the model to the predications output.
+    """
+    # Get the index of the Date:Delay signature
+    idx = self.predictions.index[self.predictions['Date:Delay'] == f'{T}:{D}'].tolist()[0]
+    self.predictions.loc[idx,'Count'] = Y_hat
+    self.predictions.loc[idx,'Actual'] = Y_test
+  
   def get_sample_data(self):
     pass
 
-  def output_prediction_score(self):
-    pass
+  def output_csv(self, model):
+    file = f'{model}_predictions.csv'
+    folder = './output'
 
+    if not os.path.exists(folder):
+      os.mkdir(folder)
+    full_path = os.path.join(folder, file)   
+
+    output_to_upload = self.predictions[['Date:Delay', 'Count']]
+    output_to_upload.to_csv(full_path, index=False)
+
+
+
+model = Backtest()
+RMSPE = model(output_predication_csv = False, model='regression')
+print(f'RMSPE: {RMSPE}')
--- wastewater/test_model.py
+++
@@ -1,8 +0,0 @@
-from backtesting import Backtest
-from sklearn.linear_model import LinearRegression
-
-def model(train, test):
-  pass
-
-test = Backtest()
-test(LinearRegression())
